cfb9b6ea327c511c5ea8f808e50ce3fb01ab6173
Edwin <46379855+Ebi2626@users.noreply.github.com>
2019-08-18 20:37:33 +0200

    Add files via upload

--- precache-manifest.21ef84e67d5e3c5ce3747ab4620eabc7.js
+++ precache-manifest.21ef84e67d5e3c5ce3747ab4620eabc7.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "9dac2d96387cb7f78d6cf4ab3681dc85",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "2d673a3248e3d80bfcc2",
-    "url": "/static/css/main.4a04c5eb.chunk.css"
+    "url": "./static/css/main.4a04c5eb.chunk.css"
   },
   {
     "revision": "627994abde0d2ec6efa3",
-    "url": "/static/js/2.3543812d.chunk.js"
+    "url": "./static/js/2.3543812d.chunk.js"
   },
   {
     "revision": "2d673a3248e3d80bfcc2",
-    "url": "/static/js/main.c1a54828.chunk.js"
+    "url": "./static/js/main.c1a54828.chunk.js"
   },
   {
     "revision": "4f5dc3cc267a2e4b2bbe",
-    "url": "/static/js/runtime~main.050c13df.js"
+    "url": "./static/js/runtime~main.050c13df.js"
   },
   {
     "revision": "e28975521df42a470a60593872337098",
-    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+    "url": "./static/media/Pixel-LCD-7.e2897552.woff"
   },
   {
     "revision": "2810fe6b5435e75b3dad63e341b97d02",
-    "url": "/static/media/bg.2810fe6b.png"
+    "url": "./static/media/bg.2810fe6b.png"
   }
 ]);
